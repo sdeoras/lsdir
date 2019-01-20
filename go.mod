1146f59c32f832078c4c5c4ef21ec14de22f3898
Saurabh Deoras <sdeoras@gmail.com>
2019-01-20 08:31:41 -0800

    allow to set recursion and pattern
    Signed-off-by: Saurabh Deoras <sdeoras@gmail.com>

--- go.mod
+++ go.mod
@@ -1,6 +1,3 @@
 module github.com/sdeoras/lsdir
 
-require (
-	github.com/google/uuid v1.1.0
-	github.com/prometheus/common v0.0.0-20190107103113-2998b132700a
-)
+require github.com/google/uuid v1.1.0
--- lister.go
+++ lister.go
@@ -15,17 +15,32 @@ type Lister interface {
 
 // lister implements Lister
 type lister struct {
+	pattern string
+	recurse bool
 }
 
-// New creates a new instance of lister
-func NewLister() Lister {
-	return &lister{}
+// New creates a new instance of lister using a pattern that matches using
+// shell wildcards (and not regexp). recurse indicates if recursive search is on.
+func NewLister(recurse bool, pattern string) Lister {
+	l := new(lister)
+	l.pattern = pattern
+	l.recurse = recurse
+
+	return l
 }
 
 func (l *lister) List(paths ...string) ([]string, error) {
 	out := make(map[string]struct{})
+	var listFunc func(path string) ([]string, error)
+
+	if l.recurse {
+		listFunc = list
+	} else {
+		listFunc = listNonRecursive
+	}
+
 	for _, path := range paths {
-		files, err := list(path)
+		files, err := listFunc(path)
 		if err != nil {
 			return nil, err
 		}
@@ -36,12 +51,43 @@ func (l *lister) List(paths ...string) ([]string, error) {
 
 	files := make([]string, 0, len(out))
 	for file := range out {
-		files = append(files, file)
+		if matched, err := filepath.Match(l.pattern, filepath.Base(file)); matched && err == nil {
+			files = append(files, file)
+		} else if err != nil {
+			return nil, err
+		}
 	}
 
 	sort.Strings(files)
 
 	return files, nil
+}
+
+func listNonRecursive(path string) ([]string, error) {
+	fileInfo, err := os.Stat(path)
+	if err != nil {
+		return nil, err
+	}
+
+	var out []string
+	if !fileInfo.IsDir() {
+		out = append(out, path)
+		return out, nil
+	}
+
+	files, err := ioutil.ReadDir(path)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, file := range files {
+		file := file
+		if !file.IsDir() {
+			out = append(out, filepath.Join(path, file.Name()))
+		}
+	}
+
+	return out, nil
 }
 
 // list is an internal function that is called recursively
